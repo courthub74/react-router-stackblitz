c7ad167aee4f2d159936f88978a0bb2d6aa03693
courthub74 <courdevelops@gmail.com>
2022-01-03 20:11:22 -0500

    Catalog Logic added not working

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
-export default function Catalog() {
+export default function Catalog(props) {
+  // if (props.match.parms.user) {
+  //   const user = props.match.params.user;
+  //   const message = props.match.params.msg;
+  //   console.log(props.match);
+
+  //   return (
+  //     <>
+  //       <h2>Catalog for: {user}</h2>
+  //       <p>This is the message: {message}</p>
+  //     </>
+  //   );
+  // }
   return <h2>Catalog Page</h2>;
 }
